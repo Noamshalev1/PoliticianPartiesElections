533035e76a15c5c3dee518807bf864a8f8051ffc
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-03-17 15:14:37 +0200

    Update Politician.h

--- Politician.h
+++ Politician.h
@@ -1,79 +1,42 @@
-/* Politician class implementations */
-#include "Politician.h"
+/* Politician class:
+ * Has the details about the politician and his party (if exist)
+ * This class is abstract - the print function is pure virtual!
+ * All the not virtual functions implemented only in Politician.cpp */
+#ifndef POLITICIAN_H
+#define POLITICIAN_H
+#include "Party.h"
+#include <string>
+#include <iostream>
+using namespace std;
+/* All the inherits classes have thous fields */
+class Politician{
+protected:
+    string _firstName;
+    string _lastName;
+    int _id;
+    Party* _party;
+    Politician* _headOfParty;
+    int _power;
+public:
+    Politician(); // default constructor
+    Politician(const string& first, const string& last, int id, int power); // constructor of create new politician
+    virtual ~Politician(); // destructor - must be virtual for his inherits
+    void updateHeadOfParty(); // update the head of party
+    void updateParty(Party* pParty); // update when the politician added to a new party
+    bool operator==(const Politician&)const; // compare between 2 politician
+    virtual int getElectionsPower()const; // virtual function - for social politician it's implemented in his class
+    virtual int getPower()const; // get the original power of the leader politician, for social return -1
+    int getId()const; // get the id of politician (for deletion option)
+    bool isExistParty(); // check if the politician has a party
+    int getPartySize()const; // get the size of politician party (if exist)
+    Party* getParty()const; // return a pointer to the politician party
+    const string& getFirstname()const; // return the politician first name (for printing the head)
+    virtual void printPolitician()const=0; // pure virtual - implemented only in the inherits.
+    virtual bool canJoinRepublicanParty()=0; // these functions will implement only in Republican and Democrat Politician
+    virtual bool canJoinDemocratParty()=0;
 
-/* Default constructor. Initialized fields */
-Politician::Politician()
-{
-    _firstName = "";
-    _lastName = "";
-    _id = 0;
-    _party = nullptr;
-    _headOfParty = nullptr;
-    _power = 0;
-}
 
-/* Constructor with details about the politician */
-Politician::Politician(const string& first, const string& last, int id, int power)
-{
-    _firstName = first;
-    _lastName = last;
-    _id = id;
-    _party = nullptr; // for the meantime he just added to system
-    _headOfParty = nullptr;
-    _power = power;
+};
 
-}
-/* Default destructor */
-Politician::~Politician(){
-}
-/* Update the head of the current party of the politician  */
-void Politician::updateHeadOfParty()
-{
-    _headOfParty = _party->getHeadOfParty();
-}
-/* Update a new party for the politician (and also the head)
- * if we just remove his party, we put null */
-void Politician::updateParty(Party* pParty)
-{
-    _party = pParty;
-    if(_party != nullptr)
-        updateHeadOfParty();
-    else
-        _headOfParty = nullptr;
-}
-/* Compare between 2 politicians by the id (uniq) */
-bool Politician::operator==(const Politician & other) const
-{
-    return (_id == other._id);
-}
-/* Return the power of the politician -
- * For social politician its different so implemented at his class */
-int Politician::getElectionsPower() const{
-    return _power;
-}
-/* Return the original power of the politician */
-int Politician::getPower() const{
-    return _power;
-}
-/* Return the politician's id */
-int Politician::getId() const{
-    return _id;
-}
-/* Return the politician's first name */
-const string& Politician::getFirstname() const {
-    return _firstName;
-}
-/* Return if the politician has a party */
-bool Politician::isExistParty() {
-    if(_party == nullptr)
-        return false;
-    return true;
-}
-/* Return the size of the politician's party (guessing there is) */
-int Politician::getPartySize() const {
-    return _party->getSize();
-}
-/* Return a pointer to the politician's party */
-Party* Politician::getParty() const {
-    return _party;
-}
+
+#endif //POLITICIAN_H
